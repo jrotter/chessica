725f5d3d87f2d492298b82352bf08d42ba5e4da6
Jeremy Rotter <jeremy.rotter@gmail.com>
2017-11-16 23:00:08 -0500

    Helpers

---
+++ src/helpers/helpers.h
@@ -0,0 +1,8 @@
+#ifndef helpers_h__
+#define helpers_h__
+
+//io_helpers.cpp
+extern int TextToSquareNumber(char Text[]);
+extern int SquareNumberToText(int SquareNumber, char Text[]);
+
+#endif  // helpers_h__
